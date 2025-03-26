512d7c1fe70ca45ed347316682a8eb381b2a1a7a
SA17 <108982682+SA-17@users.noreply.github.com>
2025-03-26 15:44:41 +0000

    Update b.sh

--- b.sh
+++ b.sh
@@ -1,5 +1,5 @@
 sudo apt-get update-y
-sudo apt install aircrack-ng python3 hashcat -y
+sudo apt install aircrack-ng python3 -y
 git clone https://github.com/SA-17/capbrute.git cap
 cd cap
 python3 hy.py
